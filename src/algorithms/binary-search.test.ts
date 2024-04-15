fbad17fea43b1fb1037278bf978f882de4deaa15
Thiago Martins <thiago_valentim1@hotmail.com>
2024-04-15 17:04:55 -0300

    feat(algorithms): binary search finds the middle element

--- src/algorithms/binary-search.test.ts
+++ src/algorithms/binary-search.test.ts
@@ -9,4 +9,8 @@ describe('Binary Search', () => {
   test('returns the index of the last element of a sorted array', () => {
     expect(binarySearch([10, 20, 30], 30)).toBe(2);
   });
+
+  test('returns the index of the middle element of a sorted array', () => {
+    expect(binarySearch([10, 20, 30], 20)).toBe(1);
+  });
 });
--- src/algorithms/binary-search.ts
+++ src/algorithms/binary-search.ts
@@ -1,5 +1,39 @@
 export type Index = number;
 
-export function binarySearch(array: number[], elementToFind: number): Index {
-  return array[0] === elementToFind ? 0 : 2;
+export function binarySearch(
+  array: number[],
+  numberToFind: number,
+  indexes: {
+    start: number;
+    end: number;
+  } = {
+    start: 0,
+    end: array.length,
+  }
+): Index {
+  const middle = Math.floor((indexes.start + indexes.end) / 2);
+  const numberAtMiddle = array[middle];
+  console.log(`
+  middle: ${middle}
+  numberAtMiddle: ${numberAtMiddle}
+`);
+  if (numberAtMiddle === numberToFind) return middle;
+
+  const isGreater = numberToFind > numberAtMiddle;
+
+  console.log(`
+    isGreater: ${isGreater}
+  `);
+
+  if (isGreater) {
+    return binarySearch(array, numberToFind, {
+      start: middle + 1,
+      end: indexes.end,
+    });
+  } else {
+    return binarySearch(array, numberToFind, {
+      start: indexes.start,
+      end: middle - 1,
+    });
+  }
 }
