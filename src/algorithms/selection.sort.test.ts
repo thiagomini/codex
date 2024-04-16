3652d2bfe11288e0759544a65bc36bb6cf123b9f
Thiago Martins <thiago_valentim1@hotmail.com>
2024-04-16 15:37:03 -0300

    test(algorithms): test selection sort for empty array and duplicate values

--- src/algorithms/selection.sort.test.ts
+++ src/algorithms/selection.sort.test.ts
@@ -1,13 +1,33 @@
 import { describe, test, expect } from 'bun:test';
 import { selectionSort } from './selection.sort';
 describe('Selection Sort', () => {
-  test('sorts in descending order', () => {
+  test('sorts in DESC order', () => {
     const list = [20, 10, 30];
     expect(selectionSort(list, 'DESC')).toEqual([30, 20, 10]);
   });
 
-  test('sorts in ascending order', () => {
+  test('sorts in ASC order', () => {
     const list = [20, 10, 30];
     expect(selectionSort(list, 'ASC')).toEqual([10, 20, 30]);
+  });
+
+  test('sorts an array with duplicate values in DESC order', () => {
+    const list = [20, 10, 30, 20];
+    expect(selectionSort(list, 'DESC')).toEqual([30, 20, 20, 10]);
+  });
+
+  test('sorts an array with duplicate values in ASC order', () => {
+    const list = [20, 10, 30, 20];
+    expect(selectionSort(list, 'ASC')).toEqual([10, 20, 20, 30]);
+  });
+
+  test('sorts an empty array in DESC order', () => {
+    const list: number[] = [];
+    expect(selectionSort(list, 'DESC')).toEqual([]);
+  });
+
+  test('sorts an empty array in ASC order', () => {
+    const list: number[] = [];
+    expect(selectionSort(list, 'ASC')).toEqual([]);
   });
 });
