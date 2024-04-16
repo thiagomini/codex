a7cdd1db31eb50842e390ddc07048c4b2f41a616
Thiago Martins <thiago_valentim1@hotmail.com>
2024-04-16 17:34:08 -0300

    feat(algorithms): implement optimized selection sort using a set

--- src/algorithms/selection.sort.test.ts
+++ src/algorithms/selection.sort.test.ts
@@ -1,33 +1,66 @@
 import { describe, test, expect } from 'bun:test';
-import { selectionSort } from './selection.sort';
+import { selectionSort, selectionSortOpt } from './selection.sort';
 describe('Selection Sort', () => {
-  test('sorts in DESC order', () => {
-    const list = [20, 10, 30];
-    expect(selectionSort(list, 'DESC')).toEqual([30, 20, 10]);
-  });
+  describe('naive implementation', () => {
+    test('sorts in DESC order', () => {
+      const list = [20, 10, 30];
+      expect(selectionSort(list, 'DESC')).toEqual([30, 20, 10]);
+    });
 
-  test('sorts in ASC order', () => {
-    const list = [20, 10, 30];
-    expect(selectionSort(list, 'ASC')).toEqual([10, 20, 30]);
-  });
+    test('sorts in ASC order', () => {
+      const list = [20, 10, 30];
+      expect(selectionSort(list, 'ASC')).toEqual([10, 20, 30]);
+    });
 
-  test('sorts an array with duplicate values in DESC order', () => {
-    const list = [20, 10, 30, 20];
-    expect(selectionSort(list, 'DESC')).toEqual([30, 20, 20, 10]);
-  });
+    test('sorts an array with duplicate values in DESC order', () => {
+      const list = [20, 10, 30, 20];
+      expect(selectionSort(list, 'DESC')).toEqual([30, 20, 20, 10]);
+    });
 
-  test('sorts an array with duplicate values in ASC order', () => {
-    const list = [20, 10, 30, 20];
-    expect(selectionSort(list, 'ASC')).toEqual([10, 20, 20, 30]);
-  });
+    test('sorts an array with duplicate values in ASC order', () => {
+      const list = [20, 10, 30, 20];
+      expect(selectionSort(list, 'ASC')).toEqual([10, 20, 20, 30]);
+    });
 
-  test('sorts an empty array in DESC order', () => {
-    const list: number[] = [];
-    expect(selectionSort(list, 'DESC')).toEqual([]);
-  });
+    test('sorts an empty array in DESC order', () => {
+      const list: number[] = [];
+      expect(selectionSort(list, 'DESC')).toEqual([]);
+    });
 
-  test('sorts an empty array in ASC order', () => {
-    const list: number[] = [];
-    expect(selectionSort(list, 'ASC')).toEqual([]);
+    test('sorts an empty array in ASC order', () => {
+      const list: number[] = [];
+      expect(selectionSort(list, 'ASC')).toEqual([]);
+    });
+  });
+  describe('optimized implementation', () => {
+    test('sorts in DESC order', () => {
+      const list = [20, 10, 30];
+      expect(selectionSortOpt(list, 'DESC')).toEqual([30, 20, 10]);
+    });
+
+    test('sorts in ASC order', () => {
+      const list = [20, 10, 30];
+      expect(selectionSortOpt(list, 'ASC')).toEqual([10, 20, 30]);
+    });
+
+    test('sorts an array with duplicate values in DESC order', () => {
+      const list = [20, 10, 30, 20];
+      expect(selectionSortOpt(list, 'DESC')).toEqual([30, 20, 20, 10]);
+    });
+
+    test('sorts an array with duplicate values in ASC order', () => {
+      const list = [20, 10, 30, 20];
+      expect(selectionSortOpt(list, 'ASC')).toEqual([10, 20, 20, 30]);
+    });
+
+    test('sorts an empty array in DESC order', () => {
+      const list: number[] = [];
+      expect(selectionSortOpt(list, 'DESC')).toEqual([]);
+    });
+
+    test('sorts an empty array in ASC order', () => {
+      const list: number[] = [];
+      expect(selectionSortOpt(list, 'ASC')).toEqual([]);
+    });
   });
 });
--- src/algorithms/selection.sort.ts
+++ src/algorithms/selection.sort.ts
@@ -21,3 +21,33 @@ export function selectionSort(list: number[], order: 'ASC' | 'DESC'): number[] {
 
   return result;
 }
+
+export function selectionSortOpt(list: number[], order: 'ASC' | 'DESC') {
+  debugger;
+  const availableIndexesMap = new Set<number>(list.map((_n, i) => i));
+  const sorted: number[] = [];
+
+  while (sorted.length < list.length) {
+    let indexOfMaxOrMin = -1;
+    let maxOrMinValue = order === 'ASC' ? Infinity : -Infinity;
+
+    for (const index of availableIndexesMap) {
+      if (availableIndexesMap.size === 1) {
+        indexOfMaxOrMin = index;
+      }
+      const valueAtIndex = list[index];
+      if (
+        (order === 'DESC' && valueAtIndex >= maxOrMinValue) ||
+        (order === 'ASC' && valueAtIndex <= maxOrMinValue)
+      ) {
+        indexOfMaxOrMin = index;
+        maxOrMinValue = list[index];
+      }
+    }
+
+    sorted.push(list[indexOfMaxOrMin]);
+    availableIndexesMap.delete(indexOfMaxOrMin);
+  }
+
+  return sorted;
+}
