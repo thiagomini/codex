e32e1a693d7b86dfc72bc7c17134ff99e5e58d88
Thiago Martins <thiago_valentim1@hotmail.com>
2024-04-17 10:16:50 -0300

    chore(natives): remove unnecessary imports

--- src/natives/array.test.ts
+++ src/natives/array.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from 'bun:test';
+import { describe, test } from 'bun:test';
 
 describe('Array', () => {
   test('should remove from few elements with O(1)', () => {
--- src/natives/set.test.ts
+++ src/natives/set.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect } from 'bun:test';
+import { describe, test } from 'bun:test';
 
 describe('Set', () => {
   test('should remove from few elements with O(1)', () => {
