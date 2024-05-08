f61a0d09264c0549f6dd9c47e8a2e1d35ffdbd45
Thiago Martins <thiago_valentim1@hotmail.com>
2024-05-08 14:37:26 -0300

    feat(data-structures): add AVL Tree

--- src/data-structures/avl-tree.test.ts
+++ src/data-structures/avl-tree.test.ts
@@ -1,33 +1,29 @@
 import { describe, expect, test } from 'bun:test';
-import { BSTree } from './binary-search-tree';
-import { balance } from './avl-balance.strategy';
+import { AVLTree } from './avl-tree';
 
-describe('AVL Balance Strategy', () => {
+describe('AVL Tree', () => {
   describe('tree of height 2', () => {
-    test('perform left-rotation when balance factor is -2', () => {
+    test('perform left-rotation for LEFT-LEFT case', () => {
       // Arrange
-      const bst = new BSTree();
-      bst.addMany(2, 1, 0);
+      const avl = new AVLTree();
 
       // Act
-      balance(bst, 2);
+      avl.addMany(2, 1, 0);
 
       // Assert
-      const root = bst.root;
-      expect(root?.isRoot()).toBeTrue();
-      expect(root).toEqual(node({ value: 1, left: 0, right: 2 }));
+      const root = avl.root;
+      expect(root).toEqual(node({ value: 1, left: 0, right: 2, height: 1 }));
     });
-    test('perform right-rotation when balance factor is +2', () => {
+    test('perform right-rotation for RIGHT-RIGHT case', () => {
       // Arrange
-      const bst = new BSTree();
-      bst.addMany(2, 1, 3, 4);
+      const avl = new AVLTree();
 
       // Act
-      balance(bst, 2);
+      avl.addMany(2, 1, 3, 4, 5);
 
       // Assert
-      const root = bst.root;
-      expect(root).toEqual(node({ value: 3, left: 2, right: 4 }));
+      const root = avl.root;
+      expect(root).toEqual(node({ value: 2, left: 1, right: 4, height: 2 }));
     });
   });
 });
@@ -37,11 +33,13 @@ function node({
   parent,
   left,
   right,
+  height,
 }: {
   value: number;
   parent?: number;
   left?: number;
   right?: number;
+  height?: number;
 }) {
   const expectedValue = {
     value,
@@ -59,6 +57,11 @@ function node({
   right !== undefined &&
     Object.assign(expectedValue, {
       right: expect.objectContaining({ value: right }),
+    });
+
+  height !== undefined &&
+    Object.assign(expectedValue, {
+      height,
     });
   return expect.objectContaining(expectedValue);
 }
---
+++ src/data-structures/avl-tree.ts
@@ -0,0 +1,114 @@
+export class AVLTree {
+  public root?: AVLNode;
+
+  public addMany(...values: number[]): void {
+    values.forEach((v) => this.add(v));
+  }
+
+  public add(value: number): AVLNode {
+    this.root = this.createNode(value, this.root as AVLNode);
+    return this.root;
+  }
+
+  public find(value: number): AVLNode | undefined {
+    return this.findRecursive(value, this.root);
+  }
+
+  public pathTo(value: number): number[] {
+    return this.pathToNode(value, this.root as AVLNode);
+  }
+
+  private findMaxFrom(subtreeRootNode?: AVLNode): AVLNode | undefined {
+    if (!subtreeRootNode) return undefined;
+
+    if (subtreeRootNode.right) return this.findMaxFrom(subtreeRootNode.right);
+    return subtreeRootNode;
+  }
+
+  private createNode(newValue: number, nodeToCompare?: AVLNode): AVLNode {
+    if (!nodeToCompare) return new AVLNode(newValue);
+
+    if (newValue < nodeToCompare.value) {
+      nodeToCompare.left = this.createNode(newValue, nodeToCompare.left);
+    } else if (newValue > nodeToCompare.value) {
+      nodeToCompare.right = this.createNode(newValue, nodeToCompare);
+    } else {
+      return nodeToCompare;
+    }
+
+    const balance = nodeToCompare.balanceFactor();
+
+    // Left Left Case
+    if (balance < -1 && newValue < (nodeToCompare.left?.value as number)) {
+      return this.rotateRight(nodeToCompare);
+    }
+
+    return nodeToCompare;
+  }
+
+  private rotateRight(node: AVLNode): AVLNode {
+    const newRoot = node.left as AVLNode;
+    const T2 = newRoot?.right;
+
+    newRoot.right = node;
+    node.left = T2;
+
+    return newRoot;
+  }
+
+  private findRecursive(value: number, node?: AVLNode): AVLNode | undefined {
+    if (!node) return undefined;
+    if (value === node.value) return node;
+    if (value < node.value) return this.findRecursive(value, node.left);
+    else return this.findRecursive(value, node.right);
+  }
+
+  private pathToNode(value: number, nodeToCompare?: AVLNode): number[] {
+    if (!nodeToCompare) return [];
+    if (value === nodeToCompare.value) return [nodeToCompare.value];
+    if (value > nodeToCompare.value) {
+      if (nodeToCompare.right === undefined) return [];
+      return [nodeToCompare.value].concat(
+        this.pathToNode(value, nodeToCompare.right as AVLNode)
+      );
+    } else {
+      if (nodeToCompare.left === undefined) return [];
+      return [nodeToCompare.value].concat(
+        this.pathToNode(value, nodeToCompare.left as AVLNode)
+      );
+    }
+  }
+}
+
+export class AVLNode {
+  constructor(
+    public readonly value: number,
+    public left?: AVLNode,
+    public right?: AVLNode
+  ) {}
+
+  get height(): number {
+    if (this.isLeaf()) {
+      return 0;
+    } else {
+      return Math.max(this.left?.height ?? 0, this.right?.height ?? 0) + 1;
+    }
+  }
+
+  public isLeaf() {
+    return this.left === undefined && this.right === undefined;
+  }
+
+  public updateValue(newValue: number) {
+    Object.assign(this, {
+      value: newValue,
+    });
+  }
+
+  public balanceFactor(): number {
+    const leftValue = this.left ? this.left.height + 1 : 0;
+    const rightValue = this.right ? this.right.height + 1 : 0;
+
+    return rightValue - leftValue;
+  }
+}
